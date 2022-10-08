a32e5fd9f1940fe52671f1717e6dc0d7c16bb697
Carlos Ramirez <c.eduardo.ramirez.h@gmail.com>
2022-10-08 20:04:14 +0200

    update version number

--- group_images/__init__.py
+++ group_images/__init__.py
@@ -4,7 +4,7 @@ from .interface_file import (cluster_images, parse_conf_file, run_by_conf_file,
 
 __author__ = "Carlos Ramirez"
 __email__ = "c.eduardo.ramirez.h@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = ['save_images', 'cluster_images', 'parse_conf_file', 'run_by_conf_file',
            '__author__', '__email__', '__version__']
